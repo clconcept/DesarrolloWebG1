60a9256f99ce5b7d80e744aef889b61e27bd6601
lugeminai <70250636+lugeminai@users.noreply.github.com>
2021-02-11 16:29:13 -0500

    Update gestcursos.component.ts

--- src/app/pages/admin/gestcursos/gestcursos.component.ts
+++ src/app/pages/admin/gestcursos/gestcursos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CursoService } from 'src/app/services/curso.service';
+
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-gestcursos',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestcursos.component.css']
 })
 export class GestcursosComponent implements OnInit {
+  cursos = [];
 
-  constructor() { }
+  constructor(private readonly cursoService: CursoService, 
+               
+    private activateRoute: ActivatedRoute) { }
+
+    getCursos(){
+      this.cursoService.getCursos().subscribe((rest: any) => {
+        this.cursos = rest.data;
+        console.log(this.cursos);
+      })
+    }
+  
+    getCursoById(id: number){
+      this.cursoService.getCursos().subscribe((rest: any) => {
+        this.cursos = rest.data.filter((item: { id: number; }) => item.id == id);
+        console.log(this.cursos);
+      })
+    }
+  
 
   ngOnInit(): void {
+    this.activateRoute.params.subscribe((params: Params) => {
+      if (params.id){
+        this.getCursoById(params.id);
+               
+      }
+      else {
+        this.getCursos();
+      }
+    });
   }
 
 }
